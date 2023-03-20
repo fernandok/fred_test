633265e68e9fe3d73d84cbf1a7e52cd8d6fd4e01
Fernando Khumlo <fernandok@users.noreply.github.com>
2023-03-20 18:45:30 +0530

    .

--- db-nossl-connect.php
+++ db-nossl-connect.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set ('error_reporting', E_ALL);
+ini_set ('display_errors', '1');
+error_reporting (E_ALL|E_STRICT);
+
 $host = 'vnet-simple-database.mysql.database.azure.com';
 $username = 'vnetadmin';
 $password = "Password@123";
